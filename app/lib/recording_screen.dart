1b3841f045f2163f08456e778d7ac15383828e13
Martin Langbecker <Martin.Langbecker@gmail.com>
2020-12-22 14:59:24 +0100

    added recording screen

---
+++ app/lib/recording_screen.dart
@@ -0,0 +1,19 @@
+import 'package:app/app_drawer.dart';
+import 'package:flutter/material.dart';
+
+class RecordingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Voice Recorder'),
+        ),
+        drawer: AppDrawer(),
+        body: Center(
+          child: Text('Voice recorder coming soon!™'),
+        ),
+      ),
+    );
+  }
+}
